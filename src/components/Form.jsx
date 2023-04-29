8d11aa767ecd086d43b9bf499f8244b74dc65c5f
ignacio2194 <82918800+ignacio2194@users.noreply.github.com>
2023-04-28 23:51:43 -0300

    mejoro el bug pero todavia esta

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,8 +6,8 @@ import sendEmail from "@/pages/api/send-email";
 import styles from "@/styles/Home.module.css";
 import { AiOutlineSend } from "react-icons/ai";
 import Sppinner from "@/components/Sppinner";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 function BasicExample() {
   const [values, setValues] = useState({
     email: "",
@@ -15,19 +15,17 @@ function BasicExample() {
     message: "",
   });
   const [showMe, setNotShowme] = useState(false);
-  const [resultRegex ,setResultRegex]=useState(false)
-  const regex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i
-  ;
-
-  // function to send email if this params is ok so show me a tost 
+  const [resultRegex, setResultRegex] = useState(false);
+  const regex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+  // function to send email if this params is ok so show me a tost
   const handler = async (e) => {
-    e.preventDefault();
- 
+    e.preventDefault()
+   
     try {
       const req = await sendEmail(values.email, values.subject, values.message);
       if (req.status === 200) {
-        setNotShowme(false)
-        toast.success('Email sent!', {
+        setNotShowme(false);
+        toast.success("Email sent!", {
           position: "bottom-right",
           autoClose: 5000,
           hideProgressBar: false,
@@ -36,14 +34,14 @@ function BasicExample() {
           draggable: true,
           progress: undefined,
           theme: "light",
-          });
-          setValues({
-            email: "",
-            subject: "",
-            message: "",
-          })
-      }else{
-        toast.error('There  was are an error, please try again later !', {
+        });
+        setValues({
+          email: "",
+          subject: "",
+          message: "",
+        });
+      } else {
+        toast.error("There  was are an error, please try again later !", {
           position: "bottom-right",
           autoClose: 5000,
           hideProgressBar: false,
@@ -52,48 +50,44 @@ function BasicExample() {
           draggable: true,
           progress: undefined,
           theme: "light",
-          });
+        });
       }
     } catch (error) {
       console.error(error.message);
     }
   };
   const handlerInputs = (e) => {
-    console.log(regex.test(e.target.value))
-    // if(regex.test(values)=== false){ 
-    //   toast.error(' please try again later !', {
-    //     position: "bottom-right",
-    //     autoClose: 5000,
-    //     hideProgressBar: false,
-    //     closeOnClick: true,
-    //     pauseOnHover: true,
-    //     draggable: true,
-    //     progress: undefined,
-    //     theme: "light",
-    //     });
-    // }else{
-    //   let result = regex.test(values)
-    //   setResultRegex(result)
-    //   console.log(resultRegex)
-    //   setValues({ ...values, [e.target.name]: e.target.value });
-    //   handler()
-    // }
-   
+
+    if (regex.test(values) === false) {
+      toast.error(" please try again later !", {
+        position: "bottom-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+    } else {
+      setResultRegex(!resultRegex);
+      handler()
   
+    }
   };
-  
+
   return (
     <div className="container container-form mt-4 mb-4">
       <h1 className={`${styles.titleSkill} text-center`}>Contact me</h1>
-      <Form onSubmit={()=> handlerInputs()}>
+      <Form onSubmit={(e) => handler(e)}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
           <Form.Control
             type="email"
             name="email"
-
+            required
             placeholder="Enter email"
-            onChange={(e) => handlerInputs(e)}
+            onChange={(e) =>  setValues({ ...values, [e.target.name]: e.target.value })}
           />
           <Form.Text className="text-muted"></Form.Text>
         </Form.Group>
@@ -104,7 +98,7 @@ function BasicExample() {
             type="text"
             placeholder="Subject"
             name="subject"
-            onChange={(e) =>  handlerInputs(e)}
+            onChange={(e) => setValues({ ...values, [e.target.name]: e.target.value })}
           />
         </Form.Group>
         <FloatingLabel controlId="floatingTextarea2" label="Comments">
@@ -113,33 +107,36 @@ function BasicExample() {
             placeholder="Leave a comment here"
             style={{ height: "100px" }}
             name="message"
-            onChange={(e) =>  handlerInputs(e)}
+            onChange={(e) =>setValues({ ...values, [e.target.name]: e.target.value })}
           />
         </FloatingLabel>
         <Button
           variant="primary"
           type="submit"
           className="mt-2 items-center"
-          
+          onClick={() => handlerInputs()}
         >
-       <p className="mb-0">
-  {showMe === false ? <AiOutlineSend/> : <Sppinner value={resultRegex} />}
-</p>
+          <p className="mb-0">
+            {showMe === false ? (
+              <AiOutlineSend />
+            ) : (
+              <Sppinner value={resultRegex} />
+            )}
+          </p>
         </Button>
-  
       </Form>
-<ToastContainer
-position="bottom-right"
-autoClose={5000}
-hideProgressBar={false}
-newestOnTop={false}
-closeOnClick
-rtl={false}
-pauseOnFocusLoss
-draggable
-pauseOnHover
-theme="light"
-/>
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </div>
   );
 }
