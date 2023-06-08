995cf3f0c3add61e07a3d927267b73d8f9d1eb2d
ignacio2194 <82918800+ignacio2194@users.noreply.github.com>
2023-06-08 20:00:28 -0300

    cositas

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,8 +6,8 @@ import sendEmail from "@/pages/api/send-email";
 import styles from "@/styles/Home.module.css";
 import { AiOutlineSend } from "react-icons/ai";
 import Sppinner from "@/components/Sppinner";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 function BasicExample() {
   const [values, setValues] = useState({
     email: "",
@@ -15,18 +15,18 @@ function BasicExample() {
     message: "",
   });
   const [showMe, setNotShowme] = useState(false);
-  const [resultRegex ,setResultRegex]=useState(false)
+  const [resultRegex, setResultRegex] = useState(false);
   const regex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}@[^\s@]+\.[^\s@]+$/;
 
-  // function to send email if this params is ok so show me a tost 
+  // function to send email if this params is ok so show me a tost
   const handler = async (e) => {
     e.preventDefault();
- 
+
     try {
       const req = await sendEmail(values.email, values.subject, values.message);
       if (req.status === 200) {
-        setNotShowme(false)
-        toast.success('Email sent!', {
+        setNotShowme(false);
+        toast.success("Email sent!", {
           position: "bottom-right",
           autoClose: 5000,
           hideProgressBar: false,
@@ -35,14 +35,14 @@ function BasicExample() {
           draggable: true,
           progress: undefined,
           theme: "light",
-          });
-          setValues({
-            email: "",
-            subject: "",
-            message: "",
-          })
-      }else{
-        toast.error('There  was are an error, please try again later !', {
+        });
+        setValues({
+          email: "",
+          subject: "",
+          message: "",
+        });
+      } else {
+        toast.error("There  was are an error, please try again later !", {
           position: "bottom-right",
           autoClose: 5000,
           hideProgressBar: false,
@@ -51,22 +51,21 @@ function BasicExample() {
           draggable: true,
           progress: undefined,
           theme: "light",
-          });
+        });
       }
     } catch (error) {
       console.error(error.message);
     }
   };
   const handlerInputs = (e) => {
-    e.preventDefault()
-    let result = regex.test(values)
-    setResultRegex(result)
-    if(result === true){ 
+    e.preventDefault();
+    let result = regex.test(values);
+    setResultRegex(result);
+    if (result === true) {
       setValues({ ...values, [e.target.name]: e.target.value });
-      handler()
-    
-    }else{
-      toast.error(' please try again later !', {
+      handler();
+    } else {
+      toast.error(" please try again later !", {
         position: "bottom-right",
         autoClose: 5000,
         hideProgressBar: false,
@@ -75,16 +74,14 @@ function BasicExample() {
         draggable: true,
         progress: undefined,
         theme: "light",
-        });
+      });
     }
-   
-  
   };
-  
+
   return (
     <div className="container container-form mt-4 mb-4">
       <h1 className={`${styles.titleSkill} text-center`}>Contact me</h1>
-      <Form onSubmit={()=> handlerInputs()}>
+      <Form onSubmit={() => handlerInputs()}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
           <Form.Control
@@ -117,30 +114,28 @@ function BasicExample() {
             onChange={(e) => handlerInputs(e)}
           />
         </FloatingLabel>
-        <Button
-          variant="primary"
-          type="submit"
-          className="mt-2 items-center"
-          
-        >
-       <p className="mb-0">
-  {showMe === false ? <AiOutlineSend/> : <Sppinner value={resultRegex} />}
-</p>
+        <Button variant="primary" type="submit" className="mt-2 items-center">
+          <p className="mb-0">
+            {showMe === false ? (
+              <AiOutlineSend />
+            ) : (
+              <Sppinner value={resultRegex} />
+            )}
+          </p>
         </Button>
-  
       </Form>
-<ToastContainer
-position="bottom-right"
-autoClose={5000}
-hideProgressBar={false}
-newestOnTop={false}
-closeOnClick
-rtl={false}
-pauseOnFocusLoss
-draggable
-pauseOnHover
-theme="light"
-/>
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </div>
   );
 }
