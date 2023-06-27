5bf3664620613fc94eb5eaff7d242d42e47864df
ignacio2194 <82918800+ignacio2194@users.noreply.github.com>
2023-06-27 13:29:18 -0300

    fixed problema del email

--- src/pages/api/send-email.js
+++ src/pages/api/send-email.js
@@ -1,8 +1,9 @@
 const CircularJSON = require('circular-json');
-import {useState} from 'react';
+
 const sendEmail = async (email, subject, message) => {
   const payload = CircularJSON.stringify({ email, subject, message });
-  const response = await fetch('http://localhost:3000/api/email', {
+
+  const response = await fetch('https://ignaciobenitezdev.vercel.app/api/email', {
     method: 'POST',
     body: payload,
     headers: {
