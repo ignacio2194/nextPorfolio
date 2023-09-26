e60cd3b5da6a179baa39f3ff0dd944c42afa2c8c
ignacio2194 <82918800+ignacio2194@users.noreply.github.com>
2023-09-26 12:57:00 -0300

    arreglos en nodemailer sin secure y arreglos en email.js

--- src/pages/api/send-email.js
+++ src/pages/api/send-email.js
@@ -1,18 +1,16 @@
-const CircularJSON = require('circular-json');
+const CircularJSON = require("circular-json");
 
 const sendEmail = async (email, subject, message) => {
   const payload = CircularJSON.stringify({ email, subject, message });
 
-  const apiUrl = process.env.NODE_ENV === 'production'
-    ? 'https://ignaciobenitezdev.vercel.app/api/email'
-    : 'http://localhost:3000/api/email';
-
+  const apiUrl =
+    process.env.NODE_ENV === "https://ignaciobenitezdev.vercel.app/api/email";
   const response = await fetch(apiUrl, {
-    method: 'POST',
+    method: "POST",
     body: payload,
     headers: {
-      'Content-Type': 'application/json'
-    }
+      "Content-Type": "application/json",
+    },
   });
   return response;
 };
