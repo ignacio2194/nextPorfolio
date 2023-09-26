3bbca6e8f3c83adc178f0bc19433b9a0ff47436e
Adhemir Sabino <adhemirsabino@gmail.com>
2023-09-26 12:57:31 -0500

    config .env

--- src/pages/api/send-email.js
+++ src/pages/api/send-email.js
@@ -3,18 +3,24 @@ const CircularJSON = require("circular-json");
 const sendEmail = async (email, subject, message) => {
   const payload = CircularJSON.stringify({ email, subject, message });
 
-  const apiUrl =
-    process.env.NODE_ENV === "production"
-      ? "https://ignaciobenitezdev.vercel.app/api/email"
-      : "http://localhost:3000/api/email";
+  // const apiUrl =
+  //   process.env.NODE_ENV === "production"
+  //     ? "https://ignaciobenitezdev.vercel.app/api/email"
+  //     : "http://localhost:3000/api/email";
+
+  // const {DB_USER, DB_PASSWORD} = process.env
+
   try {
-    const response = await fetch(apiUrl, {
-      method: "POST",
-      body: payload,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+    const response = await fetch(
+      "https://ignaciobenitezdev.vercel.app/api/email",
+      {
+        method: "POST",
+        body: payload,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
     return response;
   } catch (error) {
     console.log(error);
